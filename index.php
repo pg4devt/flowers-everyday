d924b17ead070603a16249cbfed239a9a6e3fb66
pg4devt <pg4devt@gmail.com>
2017-04-20 15:45:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,27 @@
-<<<<<<< HEAD
 <?php
-// include database configuration file
 include 'dbConfig.php';
 ?>
 
-=======
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
     <title>Flowers Everyday!</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Main CSS -->
     <link href="css/main.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="js/ie-emulation-modes-warning.js"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Custom styles for this template -->
     <link href="css/carousel.css" rel="stylesheet">
   </head>
-<!-- NAVBAR
-================================================== -->
+
   <body>
     <div class="navbar-wrapper">
       <div class="container">
@@ -60,14 +39,9 @@ include 'dbConfig.php';
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav navbar-right">
-<<<<<<< HEAD
                 <li class="active"><a href="index.php">Home</a></li>
                 <li><a href="viewCart.php">My Orders</a></li>
-=======
-                <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="#order">My Orders</a></li>
                 <li><a href="#contact">Contact</a></li>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
               </ul>
             </div>
           </div>
@@ -80,51 +54,35 @@ include 'dbConfig.php';
     <div class="col-md-2"></div>
     
     <!--Product Catalog Starts Here-->
-    <div class="col-md-8">
+    <div class="col-md-9">
       <div class="row">
         <h2 class="text-center">Featured Products</h2>
         <div class="col-md-3">
           <h4>3 I Love You Roses</h4>
           <img src="img/thumb_3iloveyou_roses.jpg" alt="3 I Love You Roses" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-<<<<<<< HEAD
-          <p class="price">Discount Price: ₱700</p>
-=======
           <p class="price">Discount Price: ₱900</p>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-1">Details</button>
         </div>
         <div class="col-md-3">
           <h4>6 Love Roses</h4>
           <img src="img/thumb_6love_roses.jpg" alt="6 Love Roses" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-<<<<<<< HEAD
-          <p class="price">Discount Price: ₱750</p>
-=======
           <p class="price">Discount Price: ₱900</p>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
         </div>
         <div class="col-md-3">
           <h4>12 Ecuadorian Orange</h4>
           <img src="img/thumb_12ecuadorian_orange.jpg" alt="12 Ecuadorian Orange" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-<<<<<<< HEAD
-          <p class="price">Discount Price: ₱800</p>
-=======
           <p class="price">Discount Price: ₱900</p>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-3">Details</button>
         </div>
         <div class="col-md-3">
           <h4>12 Ecuadorian Roses</h4>
           <img src="img/thumb_12ecuadorian_roses.jpg" alt="12 Ecuadorian Roses" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-<<<<<<< HEAD
           <p class="price">Discount Price: ₱850</p>
-=======
-          <p class="price">Discount Price: ₱900</p>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-4">Details</button>
         </div>
         <div class="col-md-3">
@@ -135,39 +93,27 @@ include 'dbConfig.php';
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-5">Details</button>
         </div>
         <div class="col-md-3">
-<<<<<<< HEAD
-          <h4>Valentine Special</h4>
-          <img src="img/thumb_12valentine_special.jpg" alt="12 Valentine Special" id="images"/>
-          <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-          <p class="price">Discount Price: ₱950</p>
-=======
           <h4>12 Valentine Special</h4>
           <img src="img/thumb_12valentine_special.jpg" alt="12 Valentine Special" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
           <p class="price">Discount Price: ₱900</p>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-6">Details</button>
         </div>
         <div class="col-md-3">
           <h4>Blooming Tulips</h4>
           <img src="img/thumb_blooming_tulips.jpg" alt="Blooming Tulips" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-<<<<<<< HEAD
           <p class="price">Discount Price: ₱750</p>
-=======
-          <p class="price">Discount Price: ₱900</p>
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-7">Details</button>          
         </div>
         <div class="col-md-3">
           <h4>Sunflower Bouquet</h4>
           <img src="img/thumb_sunflower.jpg" alt="Sunflower Bouquet" id="images"/>
           <p class="list-price text-danger">List Price: <s>₱1,000</s></p>
-<<<<<<< HEAD
           <p class="price">Discount Price: ₱800</p>
           <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-8">Details</button>
         </div>
-      </div><!--Product Catalog Ends Here-->
+      </div>
 
       <footer class="text-center" id="footer">
         <p class="pull-right"><a href="#">Back to top</a></p>
@@ -184,40 +130,11 @@ include 'dbConfig.php';
           include 'details-modal-thumb_12valentine_special.php';
           include 'details-modal-thumb_blooming_tulips.php';
           include 'details-modal-thumb_sunflower.php';
-    ?> 
-  
-=======
-          <p class="price">Discount Price: ₱900</p>
-          <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-8">Details</button>
-        </div>
-      </div>
-    
-	  <!-- FOOTER -->
-	  <footer class="text-center" id="footer">
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2017 Airris Jane Company, Inc. &middot;</p>
-      </footer>
-	</div>
-    
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-  
-<?php include 'details-modal-thumb_3iloveyou_roses.php';
-      include 'details-modal-thumb_6love_roses.php';
-      include 'details-modal-thumb_12ecuadorian_orange.php';
-      include 'details-modal-thumb_12ecuadorian_roses.php';
-      include 'details-modal-thumb_12ecuadorian_white.php';
-      include 'details-modal-thumb_12valentine_special.php';
-      include 'details-modal-thumb_blooming_tulips.php';
-      include 'details-modal-thumb_sunflower.php';
-  
- ?> 
->>>>>>> 1bc8d56c07416db317ed15e36280e797fdf39ca6
+    ?>       
+		 
     <script src="js/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
     <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
     <script src="js/holder.min.js"></script>
   </body>
 </html>
